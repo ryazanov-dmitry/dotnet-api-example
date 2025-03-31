9b70dd1a34c1dc5ff7debe03172e0edfef687ed6
ryzanov-dmitry <dimalat@gmail.com>
2025-03-31 07:24:44 +0300

    DB added

--- Data/Uav.cs
+++ Data/Uav.cs
@@ -1,7 +1,14 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace UavApi.Data;
+
+[Table("uav")]
 public class Uav
 {
+    [Column("id")]
     public int Id { get; set; }
+    [Column("name")]
     public string Name { get; set; } = string.Empty;
+    [Column("model")]
     public string Model { get; set; } = string.Empty;
 }
--- Program.cs
+++ Program.cs
@@ -7,7 +7,9 @@ using UavApi.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services
-builder.Services.AddDbContext<UavDbContext>(opt => opt.UseInMemoryDatabase("UavDb"));
+builder.Services.AddDbContext<UavDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
